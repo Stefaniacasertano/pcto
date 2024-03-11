6c704a1af0f585788d7005d5049e770db7593768
Stefaniacasertano <162986820+Stefaniacasertano@users.noreply.github.com>
2024-03-11 15:58:58 +0000

    stefa

---
+++ ex02.c
@@ -0,0 +1,8 @@
+imt main ()
+{
+    int numero = 8;
+    int numero2;
+    printf("ciao %d\n");
+    numero2=1;
+}
+
---
+++ ex03.c
@@ -0,0 +1,15 @@
+include <studio.h>
+int main ()
+{
+   int a = 5;
+   int b = 10;
+   int a;
+   int b;
+   intc;
+    printf(ïl mio numero %d %d\n", a - b, b - a);
+    scanf("%d %d %d', &a, 7b, &c);
+    printf(numeri inseriti %d %d %d\n", a+b+c, b=a+c, c+c+c),
+    
+
+
+}
