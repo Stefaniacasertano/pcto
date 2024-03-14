d6a6e65f91c8d057deed4ae6c79939a86415d325
Stefaniacasertano <162986820+Stefaniacasertano@users.noreply.github.com>
2024-03-14 15:40:58 +0000

    v

---
+++ ex098.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+double perimetro ()
+{
+    perimetro= a + b + c;
+    return perimetro;
+}
+double area(double a, double b, double c)
+{
+    double per_mezzo
+    double area;
+    double area_finale;
+    per_mezzo = perimetro(a, b, c) /2;
+    {
+    int main()
+    }
+    double l1;
+    double l2;
+    double l3;
+    printf(ïnserisci dimensioni del triangolo");
+    scanf("%lf, %lf, %lf", &l1, &l2, &l3);
+double risultato = perimetro(l1, l2, l3);
+{
+    int main()
+}
+double l1;
+double l2;
+double l3;
+printf(ïnserisci tre valori per i lati del triangolo \n");
+canf( %lf %lf %lf", &l1, &l2, &l3);
+double(risultato_perimetro = perimetro( ))
+
+}
+
+
